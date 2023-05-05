1bb1b14139e1d11c3ad42568f88220dfb4b785af
Jia Xin Tang Zhi <jxt201@exeter.ac.uk>
2023-05-05 17:08:35 +0200

    tidy up displayWeatherCondition function

--- src/script.js
+++ src/script.js
@@ -24,21 +24,26 @@ function getTime(date) {
 }
 
 function displayWeatherCondition(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#weather-description").innerHTML =
-    response.data.weather[0].description;
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement =  document.querySelector("#description")
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = response.data.name;
+  cityElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round( response.data.wind.speed);
+}
+
+axios.get(apiUrl).then(displayWeatherCondition);
+
   //   document.querySelector("#sunset").innerHTML =
   //   response.data.sys.sunset;
   //   document.querySelector("#sunrise").innerHTML =
   //   response.data.sys.sunrise;
-}
+
 
 // function formattedSunrise(props) {
 //   let date = new Date(props.time);
