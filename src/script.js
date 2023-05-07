8e097e1618083e83d2a2f4007a62a90af8d03258
Jia Xin Tang Zhi <jxt201@exeter.ac.uk>
2023-05-07 13:10:52 +0200

    display forecast data using the API

--- src/script.js
+++ src/script.js
@@ -23,56 +23,53 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-// function formatDay(timestamp) {
-//   let date = new Date(timestamp * 1000);
-//   let day = date.getDay();
-//   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-//   return days[day];
-// }
+  return days[day];
+}
 
-// function displayForecast(response) {
-//   let forecast = response.data.daily;
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
 
-//   let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 3) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>`;
+    }
+  });
 
-//   let forecastHTML = `<div class="row">`;
-//   forecast.forEach(function (forecastDay, index) {
-//     if (index < 6) {
-//       forecastHTML =
-//         forecastHTML +
-//         `
-//       <div class="col-2">
-//         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-//         <img
-//           src="http://openweathermap.org/img/wn/${
-//             forecastDay.weather[0].icon
-//           }@2x.png"
-//           alt=""
-//           width="42"
-//         />
-//         <div class="weather-forecast-temperatures">
-//           <span class="weather-forecast-temperature-max"> ${Math.round(
-//             forecastDay.temp.max
-//           )}° </span>
-//           <span class="weather-forecast-temperature-min"> ${Math.round(
-//             forecastDay.temp.min
-//           )}° </span>
-//         </div>
-//       </div>
-//   `;
-//     }
-//   });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 
-//   forecastHTML = forecastHTML + `</div>`;
-//   forecastElement.innerHTML = forecastHTML;
-// }
-
-// function getForecast(coordinates) {
-//   let apiKey = "2c93bb8c539579593628a1f398cf1b65";
-//   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-//   axios.get(apiUrl).then(displayForecast);
-// }
+function getForecast(coordinates) {
+  let apiKey = "2c93bb8c539579593628a1f398cf1b65";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayWeatherCondition(response) {
   let cityElement = document.querySelector("#city");
@@ -92,9 +89,11 @@ function displayWeatherCondition(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
-function searchCity(city) {
+function search(city) {
   let apiKey = "2c93bb8c539579593628a1f398cf1b65";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
@@ -103,7 +102,7 @@ function searchCity(city) {
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
-  searchCity(city);
+  search(city);
 }
 
 let searchForm = document.querySelector("#search-form");
@@ -113,7 +112,8 @@ let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
-searchCity("london");
+search("london");
+
 
 
 // function searchLocation(position) {
