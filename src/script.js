5a24d5153125492fc72a026bc4ead5c2a6d1df07
Jia Tang Zhi <72444963+jiatangzhi@users.noreply.github.com>
2023-05-09 01:10:19 +0200

    Update forecast api script.js

--- src/script.js
+++ src/script.js
@@ -71,7 +71,7 @@ function displayForecast(response) {
 
 //call forecast API
 function getForecast(coordinates) {
-  let apiKey = "a95c2c6739994ba4903e007ee817e7d1";
+  let apiKey = "c6f8ef4575250284954db9f4dfa7a996";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
